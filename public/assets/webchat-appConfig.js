b9f3bc027055771ea47b4c1af7997a80101be3b0
Ramon Rocha <ramon.rocha@compasso.com.br>
2022-12-27 16:42:30 -0300

    add app

--- public/assets/webchat-appConfig.js
+++ public/assets/webchat-appConfig.js
@@ -3,17 +3,10 @@ var appConfig = {
     accountSid: "AC8538b93f969cbd14b4a2abb65ea74d39", 
     flexFlowSid: "FOd9a98e3825db5bea31197b4c544cadc8",
 
-    pluginService: {
-        enabled: true,
-        url: '/plugins',
-      },
-      ytica: false,
-      logLevel: 'debug',
-      showSupervisorDesktopView: true,
-    
+       
     colorTheme: {
         overrides: brandedColors
     },
     startEngagementOnInit: true, 
-    lll   
+  
 }
